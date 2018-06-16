c099b4d6280f7b1e1f74f7ea149ff53661c997c2
Zoldur <zoldur99@gmail.com>
2018-06-16 03:20:47 -0700

    Get IP

--- salen_install.sh
+++ salen_install.sh
@@ -11,7 +11,7 @@ COIN_NAME='Salen'
 COIN_PORT=9102
 RPC_PORT=9012
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -165,7 +165,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
